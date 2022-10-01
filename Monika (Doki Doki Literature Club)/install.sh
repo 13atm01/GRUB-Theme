55bf3d0f7c1186f4ce07eee2ee39dc62e4f7759e
ジョン W. B <johnb@ciencias.unam.mx>
2022-10-01 01:46:55 -0500

    Add DDLC themes

--- Monika (Doki Doki Literature Club)/install.sh
+++ Monika (Doki Doki Literature Club)/install.sh
@@ -6,7 +6,7 @@
 
 ROOT_UID=0
 THEME_DIR="/usr/share/grub/themes"
-THEME_NAME="Miku-Christmas-Version"
+THEME_NAME="Monika-DDLC-Version"
 
 MAX_DELAY=20                                        # max delay for user to enter root password
 
@@ -44,8 +44,8 @@ prompt () {
 }
 
 # Welcome message
-  prompt -s "\n\t        ******************************************\n\t       * Nakano Miku (Christmas) Bootloader Theme *\n\t        ******************************************\n"
-prompt -s "\n\t             GRUB Theme by Andi Trisna Mukti \n \n"
+  prompt -s "\n\t        ******************************************\n\t       * Monika (DDLC) Bootloader Theme *\n\t        ******************************************\n"
+prompt -s "\n\t             GRUB Theme by Andi Trisna Mukti and Juan Angeles \n \n"
 
 
  
