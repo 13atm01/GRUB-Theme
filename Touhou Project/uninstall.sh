d4268df227e2c54d7c5cc20cbfdf479af736934f
Andi Trisna Mukti <anditrisnamukti04@gmail.com>
2023-04-12 05:32:07 +0700

    Fix Uninstallation

--- Touhou Project/uninstall.sh
+++ Touhou Project/uninstall.sh
@@ -44,12 +44,12 @@ prompt () {
 }
 
 # Welcome message
-  prompt -s "\n\t    ****************************************************\n\t    * Touhou Project Special Edition Bootloader Theme *\n\t    ***************************************************\n
-                                 GRUB Theme
+  prompt -s "\n\t   ***************************************************\n\t   * Touhou Project Special Edition Bootloader Theme *\n\t   ***************************************************\n
+                               GRUB Theme
 
-                                     by
+                                   by
 
-                 Andi Trisna Mukti and Juan Angeles Hernandez \n \n"
+               Andi Trisna Mukti and Juan Angeles Hernandez \n \n"
 
 
  
